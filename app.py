8eb2e8a3401ef72bd0e4c4ee37fbc76c7a1bbab8
Ashaz Khan <Ashaz@theleadsconenterprises.com>
2025-12-30 12:07:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,6 @@ def predict():
     return jsonify(res.json())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
